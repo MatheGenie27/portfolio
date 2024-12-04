22914987cc111f6305a93b091685c79f3316abd6
MatheGenie27 <78275290+MatheGenie27@users.noreply.github.com>
2024-12-04 12:50:02 +0100

    added Logos

--- src/app/impressum/impressum.component.ts
+++ src/app/impressum/impressum.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-impressum',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './impressum.component.html',
   styleUrl: './impressum.component.scss'
 })
