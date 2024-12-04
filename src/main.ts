b9c4d0a003c8d18356a87f82a55659885917033b
MatheGenie27 <78275290+MatheGenie27@users.noreply.github.com>
2024-12-04 19:43:16 +0100

    routing events now invokes a scroll to top event

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,20 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
+import { routes } from './app/app.routes';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+// Bootstrap der Anwendung mit zusätzlichen Router-Konfigurationen
+bootstrapApplication(AppComponent, {
+  ...appConfig,
+  providers: [
+    ...appConfig.providers,
+    provideRouter(routes, 
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'enabled', // Stellt die Scroll-Position nach Navigation auf Anfang wieder her
+        anchorScrolling: 'enabled' // Ermöglicht das Scrollen zu Anker-Links innerhalb der Seite
+      })
+    )
+  ],
+})
+.catch((err) => console.error(err));
